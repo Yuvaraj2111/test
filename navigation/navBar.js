58b4fe339bc5bf641381fa85a3205ff1bd215a9e
Yuvaraj2111 <64225422+Yuvaraj2111@users.noreply.github.com>
2021-12-30 12:28:14 +0530

    initial commit

---
+++ navigation/navBar.js
@@ -0,0 +1,37 @@
+// import React from 'react'
+// import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
+// import {
+//     Home
+// } from '../pages'
+// import { FontAwesome } from '@expo/vector-icons';
+// import { Ionicons } from '@expo/vector-icons';
+// import { Foundation } from '@expo/vector-icons';
+// import { Yellow } from '../theme/color'
+
+// const Tab = createMaterialBottomTabNavigator()
+
+// to add more screen add tab.screen
+
+// const NavBar = () => {
+//     return (
+//         <Tab.Navigator
+//             activeColor={Yellow}
+//             inactiveColor='#EEEEEE'
+//             barStyle={{ backgroundColor: "#222831", borderColor: Yellow, borderStyle: 'dotted', borderWidth: 1, height: 70 }}
+//             initialRouteName='Home'
+//         >
+//             <Tab.Screen
+//                 name="Home"
+//                 component={Home}
+//                 options={{
+//                     tabBarLabel: 'Home',
+//                     tabBarIcon: ({ color }) => (
+//                         <FontAwesome name="home" size={26} color={color} />
+//                     ),
+//                 }}
+//             />
+//
+//         </Tab.Navigator>
+//     )
+// }
+// export default NavBar;
---
+++ navigation/userNav.js
@@ -0,0 +1,93 @@
+import React, { useContext, useEffect, useState } from "react";
+import { View, ActivityIndicator } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { Login, Signup } from "../auth";
+import { AuthContext } from "../storage/authProvider";
+import Firebase from "../storage/firebase";
+import { Home } from "../pages";
+
+const auth = Firebase.auth();
+const Stack = createNativeStackNavigator();
+const AuthStack = createNativeStackNavigator();
+
+const StackNav = () => {
+  return (
+    <Stack.Navigator initialRouteName="navbar">
+      <Stack.Screen
+        name="navbar"
+        component={Home}
+        options={{
+          headerShown: false,
+        }}
+      />
+      {/* To add new page uncomment and change */}
+      {/* <Stack.Screen
+        name="navbar"
+        component={Home}
+        options={{
+          headerShown: false,
+        }}
+      /> */}
+    </Stack.Navigator>
+  );
+};
+
+const AuthStackNav = () => {
+  return (
+    <AuthStack.Navigator initialRouteName="login">
+      <AuthStack.Screen
+        options={{
+          headerShown: false,
+        }}
+        name="login"
+        component={Login}
+      />
+      <AuthStack.Screen
+        name="signup"
+        component={Signup}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </AuthStack.Navigator>
+  );
+};
+const UserNav = () => {
+  const { user, setUser } = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // onAuthStateChanged returns an unsubscriber
+    const unsubscribeAuth = auth.onAuthStateChanged(
+      async (authenticatedUser) => {
+        try {
+          await (authenticatedUser
+            ? setUser(authenticatedUser)
+            : setUser(null));
+          setIsLoading(false);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    );
+
+    // unsubscribe auth listener on unmount
+    return unsubscribeAuth;
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return (
+    <NavigationContainer>
+      {user ? <StackNav /> : <AuthStackNav />}
+    </NavigationContainer>
+  );
+};
+export default UserNav;
